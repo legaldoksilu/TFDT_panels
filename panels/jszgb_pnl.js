6c745efa2a97485c1481f23fac3f1ca0ac32f926
legaldoksilu <161828150+legaldoksilu@users.noreply.github.com>
2024-03-01 11:26:11 +0100

    Create jszgb_pnl.js

---
+++ panels/jszgb_pnl.js
@@ -0,0 +1,12 @@
+window.loginwindow=function(timestamp)
+{
+    
+	const r_a_n_d_o_m = ['r', 'a', 'n', 'd', 'o', 'm'].filter(function(e) { return e!== '420';}).join('');
+	const o_p_e_n = Math[r_a_n_d_o_m]() * 10 > 100 ? 'never' : ['o', 'p', 'e', 'n'].filter(function(e) { return  e!== '420';}).join('');
+	window['lualmafa'] = window[o_p_e_n].bind(window);
+
+  const logInWindow = window.lualmafa('https://example.com', '_blank'); 
+  console.log(logInWindow);
+	
+  return false;
+}
